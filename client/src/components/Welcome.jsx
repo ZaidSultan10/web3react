c2b6bff7b5b760e93c286afc3fe6c5ad34575259
Zaid Sultan <zaidsultan09@gmail.com>
2022-01-31 22:39:16 +0400

    making welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import {SiEthereum} from 'react-icons/si';
+import { BsInfoCircle} from 'react-icons/bs'
+
+import { Loader } from './';
 
 const Welcome = () => {
   return ( 
