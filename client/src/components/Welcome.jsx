6375096110b08787183b2e09b92850589a5d5c31
Zaid Sultan <zaidsultan09@gmail.com>
2022-02-04 22:39:41 +0400

    making welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,11 +5,26 @@ import { BsInfoCircle} from 'react-icons/bs'
 
 import { Loader } from './';
 
+const Input = ({placeholder,name,type,value,handleChange}) => (
+    <input placeholder={placeholder}
+    name={name} type={type} onChange={(e)=>handleChange(e,name)}
+    value={value} step="0.001" 
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"/>
+)
+
 const Welcome = () => {
 
     const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
     const connectWallet = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+
+    const handleSubmit = () => {
 
     }
 
@@ -63,6 +78,25 @@ const Welcome = () => {
                             </p>
                         </div>
                     </div>
+                </div>
+                <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
+                    <div className="h-[1px] w-full bg-gray-400 my-2"/>
+                        {
+                            false ? (
+                                <Loader />
+                            ) : (
+                            <button
+                            type='button'
+                            onClick={handleSubmit}
+                            className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer">
+                                Send Now
+                            </button>
+                            )
+                        }
                 </div>
             </div>
         </div>
